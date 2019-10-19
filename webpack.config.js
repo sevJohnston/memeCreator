27b110b39bf1f965644a6fb05c00375a45df0cba
goodmari <goodm@lanecc.edu>
2019-10-18 18:05:09 -0700

    Added package.json and webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const webpack = require('webpack');
+module.exports = {
+  context: __dirname,
+  entry: {
+    general: './src/js/general.js',
+    memes: './src/js/memes.js',
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: '[name].js',
+    publicPath: '/dist/'
+  },
+  devServer: {  
+    compress: true,  port: 8080,  hot: true,
+  }, 
+  devtool: 'source-map', 
+  module: {
+    rules: [	
+      { 
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: { loader: 'babel-loader'}
+      }, 
+      { 
+        test: /\.(less|css)$/, 
+        use: [ 'style-loader', 'css-loader', 'less-loader' ]		
+      },
+      {  
+        test: /\.(svg|eot|ttf|woff|woff2)$/,  
+        loader: 'url-loader',  options: {    limit: 10000,    name: 'fonts/[name].[ext]'  }
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loaders: [
+          {
+            loader: 'url-loader', options: { limit: 10000, name: 'images/[name].[ext]'}
+          },
+        'img-loader'
+        ],
+      },
+    ],
+  },
+  plugins: [ 
+    new webpack.ProvidePlugin(
+      { 
+        jQuery: 'jquery', 
+        $: 'jquery', 
+        jquery: 'jquery' 
+      }), 
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+}
