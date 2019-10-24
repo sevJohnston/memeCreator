78bcc76f56773045f5d3bb240ee9cd0aa16e2c55
sevJohnston <severena@comcast.net>
2019-10-24 15:45:27 -0700

    completed meme

--- src/js/memes.js
+++ src/js/memes.js
@@ -16,8 +16,11 @@ class Memes {   //create a class called Memes
       this.deviceWidth = window.innerWidth;
 
       this.createCanvas();
-      this.createMeme();
+      this.createMeme();    //don't need to bind here because its in the constructor, no event handler to cause trouble
+      this.addEventListeners();
+
   }
+
 
   createCanvas(){
     this.$canvas.width = Math.min(640, this.deviceWidth - 30);
@@ -28,9 +31,10 @@ class Memes {   //create a class called Memes
     this.$context.clearRect(0, 0, this.$canvas.width, this.$canvas.height);
     this.$canvas.width = this.image.width;
     this.$canvas.height = this.image.height;
+    this.resizeCanvas(this.$canvas.height, this.$canvas.width);
     this.$context.drawImage(this.image, 0, 0);
 
-    //set up the text drawing with code from lab 3 forum
+    //set up the text drawing 
     const fontSize = ((this.$canvas.width+this.$canvas.height)/2)*4/100;
     this.$context.font = `${fontSize}pt sans-serif`;
     this.$context.textAlign = 'center';
@@ -48,66 +52,64 @@ class Memes {   //create a class called Memes
     this.$context.strokeText(bottomText, this.$canvas.width/2, this.$canvas.height*(90/100));
     this.$context.fillText(bottomText, this.$canvas.width/2, this.$canvas.height*(90/100)); 
   }
+
+  addEventListeners(){
+    /*
+    this.createMeme = this.createMeme.bind(this);   //bind this to the class for the method createMeme
+    this.$topTextInput.onkeyup = this.createMeme;
+    this.$topTextInput.onchange = this.createMeme;
+    this.$bottomTextInput.onkeyup = this.createMeme;
+    this.$bottomTextInput.onchange = this.createMeme;   //usual way
+    */
+    // OR--create an array that contains the elements
+    //and then use the arrayName.forEach method and an arrow function
+
+    this.createMeme = this.createMeme.bind(this);
+    let inputNodes = [this.$topTextInput, this.$bottomTextInput];
+    inputNodes.forEach(element =>
+      element.addEventListener('keyup', this.createMeme));
+    inputNodes.forEach(element =>
+      element.addEventListener('change', this.createMeme));
+    
+    this.$downloadButton.onclick = this.downloadMeme.bind(this);
+    this.$imageInput.onchange = this.loadImage.bind(this);
+
+    this.resizeCanvas = this.resizeCanvas.bind(this);
+  }
+
+  downloadMeme(){
+    const imageSource = this.$canvas.toDataURL('image/png');
+    this.$downloadButton.href = imageSource;
+  }
+
+  loadImage(){
+    if(this.$imageInput.files && this.$imageInput.files[0]){
+      let reader = new FileReader();
+      reader.onload = () => {
+        this.image = new Image();
+        this.image.onload = () =>{
+          this.createMeme();
+        };
+        this.image.src = reader.result;
+      };
+      reader.readAsDataURL(this.$imageInput.files[0]);
+    }
+  }
+
+  resizeCanvas(canvasHeight, canvasWidth) {
+    let height = canvasHeight;
+    let width = canvasWidth;
+    while(height > Math.min(1000, this.deviceWidth-30) && width > Math.min(1000, this.deviceWidth-30)) {
+      height /= 2;
+      width /= 2;
+    }
+    this.$canvas.style.height = `${height}px`;
+    this.$canvas.style.width = `${width}px`;
+  }
 }
 new Memes();    
 
 /*  
-  - Write the method createMeme.  It should
-    - clear the previous image from the page
-    - draw the image
-      - initialize the height and width of the canvas to the height and width of the (default) image
-      - draw the image on the context
-    - setup text drawing
-      - initialize a local constant for the font size.  Here's the calculation   
-        this.$canvas.width+this.$canvas.height)/2)*4/100;
-      - set the font of the context to `${fontSize}pt sans-serif`
-        Notice the template literal instead of concatenation!
-      - set the textAlign property to center
-      - set the textBaseline property to top
-      - set the lineWidth property to 1/5 of the fontSize
-      - set the strokeStyle (outline) property to black
-      - set the fillStyle to white
-    - draw the text
-      - get the default top and bottom text from the ui and put both in a variable
-      - make sure both of them are all caps
-      - write them on the context 
-      - don't forget to outline the text in black!
-    - add a call to this method in the constructor
-  END OF PART 1 - TEST AND DEBUG YOUR CODE - YOU SHOULD SEE THE MEME ON THE PAGE
-
-- PART 2 - Change the code as the user types
-  - Write the method addEventListeners
-    - bind this to the class for the method createMeme
-    - add the keyup event and the change event to the top and bottom text input elements
-      - You can do this in the usual way OR you could create an array that contains the elements
-        and then use the arrayName.forEach method and an arrow function
-  - Add a call to this method in the constructor
-  END OF PART 2 - TEST AND DEBUG YOUR CODE - YOU SHOULD SEE THE MEME CHANGE WHEN YOU TYPE
-
-- PART 3 - Download the meme
-  - Write the method downloadMeme
-    - declare a constant imageSource and set it to the canvas converted to data
-    - set the href attribute of the download button to the imageSource
-  - Change the addEventListers method to include downloading
-    - bind the class to the downloadMeme method
-    - add an event handler to the click event for the download button
-END OF PART 3 - TEST AND DEBUG YOUR CODE - YOU SHOULD BE ABLE TO DOWNLOAD THE MEME
-
-- PART 4 - Choose an image
-  - Write the method loadImage
-    - if there's something in the file input on the page
-      - declare and instantiate a FileReader object
-      - set it's onload hander to an anonymous function that
-        - set the image instance variable to a new image
-        - set it's onload handler to an anonymou function that
-          - calls the createMeme method
-        - set the src property of the image to the result from reading the file
-      - read the file
-  - Change the addEventListeners
-    - bind the class to the loadImage method
-    - add an event handler to the change event for the file input element on the page
-END OF PART 4 - TEST AND DEBUG YOUR CODE - YOU SHOULD BE ABLE TO PICK AN IMAGE FOR THE MEME
-
 - Part 5 - Resize the image if the user picks a really big image
   - Write the method resizeImage
     resizeCanvas(canvasHeight, canvasWidth) {
